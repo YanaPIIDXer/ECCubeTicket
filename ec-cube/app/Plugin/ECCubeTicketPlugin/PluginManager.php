c76e835bc0bc4485ee09b4a1b2be6fd3f2832c5a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-25 17:25:30 +0900

    クラスを定義しただけで多重定義になる問題を修正。 ※namespaceが間違っていたのが原因だったらしい。

--- ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
+++ ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
@@ -1,16 +1,9 @@
 <?php
-    namespace Plugin\TicketPlugin;
-    
+    namespace Plugin\ECCubeTicketPlugin;
     use Eccube\Plugin\AbstractPluginManager;
 
     // プラグインマネージャ
     class PluginManager extends AbstractPluginManager
     {
-        // インストールされた
-        public function install($config, Application $app)
-        {
-            // TODO:カテゴリ追加処理をここに書く。
-            //      ※アンインストール時にカテゴリ消去しようと思ったけど、商品情報レベルでイカれるので止めた。
-        }
     }
 ?>
