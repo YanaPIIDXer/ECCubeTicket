5c831f0fe3042728a9e6e55e0de90cdf746a37b0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-25 18:31:17 +0900

    中身を全部撤去。 （カテゴリ以外の方法でやる方法を思い付いたので。）

--- ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
+++ ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
@@ -7,26 +7,5 @@
     // プラグインマネージャ
     class PluginManager extends AbstractPluginManager
     {
-        // 有効化された
-        public function enable(array $meta, ContainerInterface $container)
-        {
-            if($container == null)
-            {
-                die("Container is null.");
-            }
-
-            $entityManager = $container->get('doctrine')->getManager();
-            if($entityManager == null)
-            {
-                die("EntityManager is null.");
-            }
-            $categoryRepository = $container->get(CategoryRepository::class);
-            if($categoryRepository == null)
-            {
-                die("CategoryRepository is null.");
-            }
-            
-            die("OK");
-        }
     }
 ?>
