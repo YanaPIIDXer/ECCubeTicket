2d17a05c7d24e00f6d34b332bb8e5dbb03b07c66
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-25 18:02:21 +0900

    とりあえずCategoryRepositoryを取得する所まで。 ※enableメソッドはテストしやすいようにdieで殺している。

--- ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
+++ ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
@@ -1,14 +1,32 @@
 <?php
     namespace Plugin\ECCubeTicketPlugin;
     use Eccube\Plugin\AbstractPluginManager;
+    use Symfony\Component\DependencyInjection\ContainerInterface;
+    use Eccube\Repository\CategoryRepository;
 
     // プラグインマネージャ
     class PluginManager extends AbstractPluginManager
     {
-        // インストールされた
-        public function install($meta = null, $app = null, ContainerInterface $container = null)
+        // 有効化された
+        public function enable(array $meta, ContainerInterface $container)
         {
-            // TODO:ここにカテゴリ追加処理を実装。
+            if($container == null)
+            {
+                die("Container is null.");
+            }
+
+            $entityManager = $container->get('doctrine')->getManager();
+            if($entityManager == null)
+            {
+                die("EntityManager is null.");
+            }
+            $categoryRepository = $container->get(CategoryRepository::class);
+            if($categoryRepository == null)
+            {
+                die("CategoryRepository is null.");
+            }
+            
+            die("OK");
         }
     }
 ?>
