3b487e509e1bdcd520d9b89098b71ff5a150357a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-07-26 12:10:13 +0900

    ・PluginManagerにマイグレーション処理を実装。 ・マイグレーションファイル作成。

---
+++ ec-cube/app/Plugin/ECCubeTicketPlugin/DoctrineMigrations/Version20200726025014.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+    namespace Plugin\ECCubeTicketPlugin\DoctrineMigrations;
+
+    use Doctrine\DBAL\Schema\Schema;
+    use Doctrine\Migrations\AbstractMigration;
+
+    /**
+     * Auto-generated Migration: Please modify to your needs!
+     */
+    final class Version20200726025014 extends AbstractMigration
+    {
+        public function up(Schema $schema) : void
+        {
+            // this up() migration is auto-generated, please modify it to your needs
+        }
+
+        public function down(Schema $schema) : void
+        {
+            // this down() migration is auto-generated, please modify it to your needs
+        }
+    }
+?>
--- ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
+++ ec-cube/app/Plugin/ECCubeTicketPlugin/PluginManager.php
@@ -2,10 +2,22 @@
     namespace Plugin\ECCubeTicketPlugin;
     use Eccube\Plugin\AbstractPluginManager;
     use Symfony\Component\DependencyInjection\ContainerInterface;
-    use Eccube\Repository\CategoryRepository;
 
     // プラグインマネージャ
     class PluginManager extends AbstractPluginManager
     {
+        // インストール
+        public function install(array $meta, ContainerInterface $container)  
+        {
+            $entityManager = $container->get('doctrine')->getManager();
+            $this->migration($entityManager->getConnection(), $meta['code']);
+        }
+
+        // アンインストール
+        public function uninstall(array $meta, ContainerInterface $container)  
+        {
+            $entityManager = $container->get('doctrine')->getManager();
+            $this->migration($entityManager->getConnection(), $meta['code'], '0');
+        }
     }
 ?>
